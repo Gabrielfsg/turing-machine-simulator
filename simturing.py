a26226d75fa649f8d431f023faeabcba18bf0a3e
Alberto <0056119@academico.ifmg.edu.br>
2023-03-30 22:39:03 -0300

    Versão base

---
+++ simturing.py
@@ -0,0 +1,27 @@
+import argparse
+
+
+#Argumentos necessários pro programa
+"""
+
+-resume (ou -r), executa o programa até o fim e depois imprime o conteúdo final na fita.
+-verbose (ou -v) execução passo a passo
+-step <n> (ou -s <n>) mostra n linhas de execução passo a passo na tela, depois abre o prompt pedindo nova opção 
+(r,v,s). Se não fornecer nenhuma opção, repete-se a última opção
+
+"""
+
+# parser = argparse.ArgumentParser(description='Simulador de Máquina de Turing ver 1.0 - IFMG 2023')
+# parser.add_argument('-r', '--arg1', help='Resume')
+# parser.add_argument('-s', '--arg2', help='Verbose')
+# parser.add_argument('-v', '--arg2', help='Verbose numerado')
+# args = parser.parse_args()
+
+
+print('Simulador de Máquina de Turing ver 1.0 - IFMG 2023')
+print('Desenvolvido como trabalho prático para a disciplina de Teoria da Computação')
+print('Autores: Alberto Gusmão e Gabriel Gondim')
+
+print('\nForneça a palavra inicial: ',end='')
+palavra = input()
+
